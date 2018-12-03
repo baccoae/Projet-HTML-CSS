a87d518a71ba66289ea70fa7645a239945a03bbb
Mikael Fructuoso <mika.fr99@hotmail.com>
2018-12-03 11:59:08 +0100

    Plateau Tournant ENDED
    J'ia finit le plateau tournant, il manque à rajouter les images et faire du css

--- OwlCarousel2-2.3.4/OwlCarousel2-2.3.4/dist/food.js
+++ OwlCarousel2-2.3.4/OwlCarousel2-2.3.4/dist/food.js
@@ -11,8 +11,25 @@ $().ready(function(){
     });*/
 
 $(".popup").click(function(){
-    popup = this.childNodes[1];
-    popup.classList.toggle("show");
+    
+    console.log($("#myBigCarousel").owlCarousel);
+    $('#myBigCarousel').trigger('stop.owl.autoplay');
+    $('#myLittleCarousel').trigger('stop.owl.autoplay');
+    $('#stop').val("Relancer le plateau");
+    $('#description-box').empty();
+    var data = $(this).attr('id');
+    var findIt = $('#definition').find('[data-popup=' + data + ']');
+    var position = $('#definition').find('[data-popup=' + data + ']').attr('data-place');
+    console.log(position);
+    $('#myBigCarousel').trigger('to.owl.carousel', position);
+    $('#myLittleCarousel').trigger('to.owl.carousel', position-5);
+    var copie = findIt.clone();
+    $('#description-box').append(copie);
+    console.log($('#description-box').find('[data-popup=' + data + ']').toggleClass("show"));
+
+    //console.log($(this).find('.popuptext').toggleClass("show"));
+    
+
 })
 
 
