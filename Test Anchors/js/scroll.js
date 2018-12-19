4692ca5b90be949760e670b96277c887e11f93d7
baccoae <baccoae@gmail.com>
2018-12-19 14:22:39 +0100

    Nav v2
    (Non Responsive / Sera transformé en Menu Burger pour Query Mobiles)

---
+++ Test Anchors/js/scroll.js
@@ -0,0 +1,54 @@
+console.log("scrollify.js has been linked.");
+
+$(document).ready(function () {
+
+$.scrollify({
+    section: "section",
+    sectionName: "section-name",
+    interstitialSection: "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1800,
+    offset: 0,
+    scrollbars: false,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll: true,
+    before: function() {},
+    after: function() {},
+    afterResize: function() {},
+    afterRender: function() {}
+});
+
+$(function() {
+    $('#sec-change1').click (function() {
+        $.scrollify.move('#sec1');
+    });
+});
+
+$(function() {
+    $('#sec-change2').click (function(e) {
+        $.scrollify.move('#sec2');
+    });
+});
+
+$(function() {
+    $('#sec-change3').click (function(e) {
+        $.scrollify.move('#sec3');
+    });
+});
+
+$(function() {
+    $('#sec-change4').click (function(e) {
+        $.scrollify.move('#sec4');
+    });
+});
+
+$(function() {
+    $('#sec-change5').click (function(e) {
+        $.scrollify.move('#sec5');
+    });
+});
+
+});
